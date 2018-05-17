410a57e4a567e1e37995528376691cacf72ab225
Arnold Noronha <arnstein87@gmail.com>
2018-05-16 20:19:48 -0400

    ...

--- eyepatch/src/androidTest/java/com/tdrhq/eyepatch/PrimitivesTest.java
+++ eyepatch/src/androidTest/java/com/tdrhq/eyepatch/PrimitivesTest.java
@@ -5,6 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PrimitivesTest {
+    private Class[] allPrimitives = new Class[] {
+          boolean.class,
+          byte.class,
+          char.class,
+          double.class,
+          float.class,
+          int.class,
+          long.class,
+          short.class
+    };
+
     @Test
     public void testGetBoxedType() throws Throwable {
         assertEquals(TypeId.get(Integer.class), Primitives.getBoxedType(TypeId.INT));
