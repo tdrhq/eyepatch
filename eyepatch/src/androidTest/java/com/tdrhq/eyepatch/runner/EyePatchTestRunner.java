a9b24edcbdff6e58d881071d114e625e55e0a29d
Arnold Noronha <arnold@tdrhq.com>
2018-05-30 16:25:21 -0400

    ...

---
+++ eyepatch/src/androidTest/java/com/tdrhq/eyepatch/runner/EyePatchTestRunner.java
@@ -0,0 +1,8 @@
+// (c) 2018 Arnold Noronha <arnold@tdrhq.com>
+
+package com.tdrhq.eyepatch.runner;
+
+public class EyePatchTestRunner {
+    public EyePatchTestRunner() {
+    }
+}
---
+++ eyepatch/src/androidTest/java/com/tdrhq/eyepatch/runner/EyePatchTestRunnerTest.java
@@ -0,0 +1,10 @@
+package com.tdrhq.eyepatch.runner;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EyePatchTestRunnerTest {
+    @Test
+    public void testPreconditions() throws Throwable {
+    }
+}
