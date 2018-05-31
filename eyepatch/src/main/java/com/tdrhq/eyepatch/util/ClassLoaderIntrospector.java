30d55c47cab0f6bd6b8705842ff353920e65e4c4
Arnold Noronha <arnold@tdrhq.com>
2018-05-31 18:14:41 -0400

    some more utilities

--- eyepatch/src/main/java/com/tdrhq/eyepatch/util/ClassLoaderIntrospector.java
+++ eyepatch/src/main/java/com/tdrhq/eyepatch/util/ClassLoaderIntrospector.java
@@ -3,6 +3,7 @@
 package com.tdrhq.eyepatch.util;
 
 import android.text.TextUtils;
+import android.util.Log;
 import dalvik.system.BaseDexClassLoader;
 import dalvik.system.DexFile;
 import dalvik.system.PathClassLoader;
@@ -32,15 +33,39 @@ public class ClassLoaderIntrospector {
         return ret;
     }
 
-    public static ClassLoader clone(ClassLoader classLoader) {
-        if (!(classLoader instanceof BaseDexClassLoader)) {
-            throw new IllegalArgumentException("needs an android classloader to clone");
+    public static List<String> getOriginalNativeLibPath(ClassLoader parent) {
+        Object dexPathList = Whitebox.getField(parent, BaseDexClassLoader.class, "pathList");
+        assert(dexPathList != null);
+
+        List<File> files = (List<File>) Whitebox.getField(dexPathList, "nativeLibraryDirectories");
+        List<String> ret = new ArrayList<>();
+        for (File file : files) {
+            ret.add(file.getAbsolutePath());
         }
+        return ret;
+    }
 
-        List<String> dexPath = getOriginalDexPath(classLoader);
-        String dexPathAsStr = TextUtils.join(":", dexPath);
-        PathClassLoader cloned = new PathClassLoader(dexPathAsStr, classLoader.getParent());
+    public static ClassLoader clone(ClassLoader classLoader) {
+        try {
+            if (!(classLoader instanceof BaseDexClassLoader)) {
+                throw new IllegalArgumentException("needs an android classloader to clone");
+            }
 
-        return cloned;
+            Class dexPathList = Class.forName("dalvik.system.DexPathList");
+            String zipSeparator = (String) Whitebox.getStaticField(dexPathList, "zipSeparator");
+            List<String> dexPath = getOriginalDexPath(classLoader);
+            String dexPathAsStr = TextUtils.join(":", dexPath);
+
+            List<String> nativeLibPath = getOriginalNativeLibPath(classLoader);
+            String nativeLibPathAsStr = TextUtils.join(":", nativeLibPath);
+
+            PathClassLoader cloned = new PathClassLoader(dexPathAsStr, nativeLibPathAsStr, classLoader.getParent());
+
+            Log.i("ClassLoaderIntrospector", "old CL before clone: " + classLoader);
+            Log.i("ClassLoaderIntrospector", "new CL after clone : " + cloned);
+            return cloned;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
