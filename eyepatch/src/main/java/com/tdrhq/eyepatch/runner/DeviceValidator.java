79c23e8eb12eee1fb143a21458f67aa16800dfa0
Arnold Noronha <arnold@tdrhq.com>
2018-06-04 12:04:17 -0400

    missing file

---
+++ eyepatch/src/main/java/com/tdrhq/eyepatch/runner/DeviceValidator.java
@@ -0,0 +1,32 @@
+// (c) 2018 Arnold Noronha <arnold@tdrhq.com>
+
+package com.tdrhq.eyepatch.runner;
+
+import android.os.Build;
+
+public class DeviceValidator {
+    private static final String SDK_INT_MESSAGE =
+            "EyePatch is only supported on Lollipop or higher devices.\n";
+
+    public static void assertDeviceIsAcceptable() {
+        if (isEyePatchTest()) {
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT < 21) {
+            throw new UnsupportedOperationException(
+                    SDK_INT_MESSAGE);
+        }
+    }
+
+    /**
+     * Even though we're not publicly supporting < Lollipop,
+     * internally we're still working on supporting it, so we want all
+     * our tests to be able to run on thse devices. So if we're
+     * running under an eyepatch test we're going to disable these
+     * checks.
+     */
+    private static boolean isEyePatchTest() {
+        return false;
+    }
+}
