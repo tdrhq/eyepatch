c97f8abe60efb6f19950878d665a63d8fca64fc4
Arnold Noronha <arnold@tdrhq.com>
2018-06-05 15:56:22 -0400

    more thorough checks with fields

--- eyepatch/src/androidTest/java/com/tdrhq/eyepatch/dexmagic/FieldsTests.java
+++ eyepatch/src/androidTest/java/com/tdrhq/eyepatch/dexmagic/FieldsTests.java
@@ -2,12 +2,14 @@ package com.tdrhq.eyepatch.dexmagic;
 
 import com.tdrhq.eyepatch.runner.EyePatchMockable;
 import com.tdrhq.eyepatch.runner.EyePatchTestRunner;
+import com.tdrhq.eyepatch.util.Whitebox;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 @EyePatchMockable({
   FieldsTests.Foo.class,
+  FieldsTests.MultipleFields.class,
 })
 @RunWith(EyePatchTestRunner.class)
 public class FieldsTests {
@@ -40,6 +42,26 @@ public class FieldsTests {
         assertEquals(20, new Foo().getNumber());
     }
 
+    @Test
+    public void testMultipleFields() throws Throwable {
+        // verify this is truly mocked:
+        assertEquals(20, MultipleFields.getNumber());
+
+        MultipleFields instance = new MultipleFields();
+        instance.f1 = 20;
+        instance.f2 = "blah";
+        instance.f3 = 30;
+
+        assertEquals(20, instance.f1);
+        assertEquals("blah", instance.f2);
+        assertEquals(30, instance.f3);
+
+        // Also check with reflection just to make sure it's in sync
+        assertEquals(20, Whitebox.getField(instance, MultipleFields.class, "f1"));
+        assertEquals("blah", Whitebox.getField(instance, MultipleFields.class, "f2"));
+        assertEquals(30, Whitebox.getField(instance, MultipleFields.class, "f3"));
+    }
+
     public static class Foo {
         int number = 0;
 
@@ -49,6 +71,16 @@ public class FieldsTests {
 
         public static int getOtherNumber() {
             return 10;
+        }
+    }
+
+    public static class MultipleFields {
+        public int f1 = 0;
+        public String f2 = null;
+        public int f3 = 0;
+
+        public static int getNumber() {
+            return 0;
         }
     }
 }
