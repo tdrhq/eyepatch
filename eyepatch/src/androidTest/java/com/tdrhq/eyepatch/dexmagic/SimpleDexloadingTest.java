ecc434e0ac5bd56007033a4254eed77ee2c1f56c
Arnold Noronha <arnold@tdrhq.com>
2018-06-05 16:52:03 -0400

    add a trivial class loading test for kitkat

---
+++ eyepatch/src/androidTest/java/com/tdrhq/eyepatch/dexmagic/SimpleDexloadingTest.java
@@ -0,0 +1,34 @@
+package com.tdrhq.eyepatch.dexmagic;
+
+import com.android.dx.DexMaker;
+import com.android.dx.TypeId;
+import dalvik.system.DexFile;
+import java.io.File;
+import java.lang.reflect.Modifier;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import static org.junit.Assert.*;
+
+/**
+ * Older API levels have some quirks with loading classes, this test
+ * tries to irons out those quirks.
+ */
+public class SimpleDexloadingTest {
+    static int counter = 0; // almost never used, but just in case
+
+    @Rule
+    public TemporaryFolder tmpdir = new TemporaryFolder();
+
+    @Test
+    public void testHappyPath() throws Throwable {
+        DexMaker dexmaker = new DexMaker();
+        String className = "com.foo.Bar" + (++counter);
+        TypeId<?> typeId = Util.createTypeIdForName(className);
+        dexmaker.declare(typeId, className + ".generated", Modifier.PUBLIC, TypeId.OBJECT);
+
+        File of = new File(tmpdir.getRoot(), "blah" + (++counter) + ".jar");
+        DexFile dexFile = Util.createDexFile(dexmaker, of);
+        dexFile.loadClass(className, getClass().getClassLoader());
+    }
+}
