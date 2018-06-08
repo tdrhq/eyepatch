24a2264ec9091d8aac17c11d198d3f1173be471b
Arnold Noronha <arnold@tdrhq.com>
2018-06-08 12:05:50 -0400

    system properties

---
+++ eyepatch/src/androidTest/java/com/tdrhq/eyepatch/runner/SystemPropertiesTest.java
@@ -0,0 +1,15 @@
+package com.tdrhq.eyepatch.runner;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class SystemPropertiesTest {
+    @Test
+    public void testSimpleCheck() throws Throwable {
+        assertThat(SystemProperties.getSystemProperty("ro.product.device"),
+                   not(isEmptyString()));
+        assertThat(SystemProperties.getSystemProperty("does.not.exist"),
+                   is(equalTo("")));
+    }
+}
---
+++ eyepatch/src/main/java/com/tdrhq/eyepatch/runner/SystemProperties.java
@@ -0,0 +1,31 @@
+// (c) 2018 Arnold Noronha <arnold@tdrhq.com>
+
+package com.tdrhq.eyepatch.runner;
+
+import android.content.Context;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class SystemProperties {
+    public static String getSystemProperty(String key) {
+        try {
+            ClassLoader cl = Context.class.getClassLoader();
+            Class<?> SystemProperties = cl.loadClass("android.os.SystemProperties");
+
+            Class[] paramTypes = { String.class };
+            Method get = SystemProperties.getMethod("get", paramTypes);
+
+            //Parameters
+            Object[] params = { key };
+            return (String) get.invoke(null, params);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
