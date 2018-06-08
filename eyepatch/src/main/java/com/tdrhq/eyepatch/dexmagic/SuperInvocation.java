f5c3cdbf0f3084fc7b52d99756481762b9d9c498
Arnold Noronha <arnold@tdrhq.com>
2018-06-08 14:08:50 -0400

    super invocation data type

---
+++ eyepatch/src/main/java/com/tdrhq/eyepatch/dexmagic/SuperInvocation.java
@@ -0,0 +1,26 @@
+// (c) 2018 Arnold Noronha <arnold@tdrhq.com>
+
+package com.tdrhq.eyepatch.dexmagic;
+
+/**
+ * This provides all the information need by the generated class to
+ * figure out what to call to super.
+ */
+public class SuperInvocation {
+    private Class[] mArgTypes;
+    private Object[] mArgs;
+
+    public Object[] getArgs() {
+        return mArgs;
+    }
+
+    public Class[] getArgTypes() {
+        return mArgTypes;
+    }
+
+    public SuperInvocation(Class[] argTypes,
+                           Object[] args) {
+        mArgTypes = argTypes;
+        mArgs = args;
+    }
+}
