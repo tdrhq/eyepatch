1658a5d8a06735f906e80412fd0d3a4796ac2b93
Arnold Noronha <arnold@tdrhq.com>
2018-06-08 15:31:27 -0400

    add new test

---
+++ eyepatch/src/androidTest/java/com/tdrhq/eyepatch/dexmagic/ConstructorGeneratorTest.java
@@ -0,0 +1,77 @@
+package com.tdrhq.eyepatch.dexmagic;
+
+import com.android.dx.Code;
+import com.android.dx.DexMaker;
+import com.android.dx.TypeId;
+import com.tdrhq.eyepatch.EyePatchTemporaryFolder;
+import com.tdrhq.eyepatch.util.Checks;
+import com.tdrhq.eyepatch.util.ClassLoaderIntrospector;
+import dalvik.system.DexFile;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Modifier;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ConstructorGeneratorTest {
+    private ClassLoader classLoader = ClassLoaderIntrospector
+            .clone(getClass().getClassLoader());
+
+    @Rule
+    public EyePatchTemporaryFolder tmpdir = new EyePatchTemporaryFolder();
+
+    DexMaker dexmaker;
+    TypeId<?> typeId;
+    Class superClass;
+
+    @Before
+    public void before() throws Throwable {
+        dexmaker = new DexMaker();
+        typeId = Util.createTypeIdForName("com.foo.ForTest");
+    }
+
+    private void declareClass(Class superClass) {
+        this.superClass = superClass;
+        dexmaker.declare(
+                typeId,
+                "com.foo.ForTest.generated",
+                Modifier.PUBLIC,
+                TypeId.get(superClass));
+    }
+
+    @Test
+    public void testPreconditions() throws Throwable {
+        declareClass(SuperClassSimpleConstructor.class);
+        Code code = dexmaker.declare(typeId.getConstructor(), Modifier.PUBLIC);
+        ConstructorGenerator generator = createGenerator(code);
+        generator.declareLocals();
+        generator.invokeSuper();
+        code.returnVoid();
+
+        Class klass = generateClass();
+        klass.newInstance();
+    }
+
+    private ConstructorGenerator createGenerator(Code code) {
+        return new ConstructorGenerator(
+                typeId,
+                superClass,
+                code);
+    }
+
+    private Class generateClass() throws IOException {
+        File of = new File(tmpdir.getRoot(),
+                           "MyClass.jar");
+        DexFile dexFile = Util.createDexFile(dexmaker, of);
+        return Checks.notNull(dexFile.loadClass("com.foo.ForTest", classLoader));
+    }
+
+    public static class SuperClassSimpleConstructor {
+        boolean invoked = false;
+        public SuperClassSimpleConstructor() {
+            invoked = true;
+        }
+    }
+}
