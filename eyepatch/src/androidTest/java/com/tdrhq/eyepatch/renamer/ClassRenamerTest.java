aa4e7a7fbe3589307c3d4bbd8ea2d07a3ef53f70
Arnold Noronha <arnold@tdrhq.com>
2018-06-14 13:31:08 -0400

    test is getting better

--- eyepatch/src/androidTest/java/com/tdrhq/eyepatch/renamer/ClassRenamerTest.java
+++ eyepatch/src/androidTest/java/com/tdrhq/eyepatch/renamer/ClassRenamerTest.java
@@ -3,9 +3,12 @@
 package com.tdrhq.eyepatch.renamer;
 
 import com.android.dx.DexMaker;
+import com.android.dx.TypeId;
 import com.tdrhq.eyepatch.EyePatchTemporaryFolder;
 import com.tdrhq.eyepatch.dexmagic.Util;
+import dalvik.system.PathClassLoader;
 import java.io.File;
+import java.lang.reflect.Modifier;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,18 +17,31 @@ import static org.junit.Assert.*;
 public class ClassRenamerTest {
     @Rule
     public EyePatchTemporaryFolder tmpdir = new EyePatchTemporaryFolder();
+    private File input;
+    private ClassLoader classLoader = new PathClassLoader("", null, getClass().getClassLoader());
 
     @Before
     public void before() throws Throwable {
-        File file = tmpdir.newFile("input.dex");
+        input = tmpdir.newFile("input.dex");
 
         // let's write a very basic Class to the file
         DexMaker dexmaker = new DexMaker();
-        Util.writeDexFile(dexmaker, file);
+        TypeId<?> typeId = Util.createTypeIdForName("com.foo.Foo");
+        dexmaker.declare(typeId, "Foo.generated", Modifier.PUBLIC,
+                         TypeId.OBJECT);
+        // let's generate a bar() method that returns a constant
+        //        MethodId method = typeId.getMethod(
+        //TypeId.STRING,
+        //"getBar");
+
+        Util.writeDexFile(dexmaker, input);
     }
 
     @Test
     public void testPreconditions() throws Throwable {
-
+        // load the original class
+        Class FooClass = Util.loadDexFile(input)
+                .loadClass("com.foo.Foo", classLoader);
+        assertNotNull(FooClass);
     }
 }
