dd4d0182cf3c0129c8ba246ad181181c2fe2dc40
Arnold Noronha <arnold@tdrhq.com>
2018-07-10 14:11:57 -0500

    use streams for performance

--- eyepatch/src/main/java/com/tdrhq/eyepatch/util/DexFileUtil.java
+++ eyepatch/src/main/java/com/tdrhq/eyepatch/util/DexFileUtil.java
@@ -5,12 +5,11 @@ package com.tdrhq.eyepatch.util;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Set;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import org.jf.dexlib2.Opcodes;
 import org.jf.dexlib2.dexbacked.DexBackedDexFile;
 import org.jf.dexlib2.iface.ClassDef;
@@ -21,12 +20,14 @@ public class DexFileUtil {
     }
 
     public static DexBackedDexFile readDexFile(File ret) throws IOException {
-        InputStream dexFileExtracted = extractDexFile(ret);
+        InputStream fis = new BufferedInputStream(new FileInputStream(ret));
+        InputStream dexFileExtracted = extractDexFile(fis);
         InputStream is = new BufferedInputStream(dexFileExtracted);
         try {
             return DexBackedDexFile.fromInputStream(Opcodes.forApi(16), is);
         } finally {
             is.close();
+            fis.close();
         }
     }
 
@@ -41,26 +42,30 @@ public class DexFileUtil {
         return null;
     }
 
-    private static InputStream extractDexFile(File input) throws IOException {
-        if (input.toString().endsWith(".dex")) {
-            return new FileInputStream(input);
+    private static InputStream extractDexFile(InputStream input) throws IOException {
+        input.mark(10);
+        if (input.read() != 0x50) {
+            input.reset();
+            return input;
         }
 
+        input.reset();
         return extractJarFile(input);
     }
 
-    private static InputStream extractJarFile(File input) throws IOException {
-        final JarFile jarFile = new JarFile(input);
-        JarEntry entry = jarFile.getJarEntry("classes.dex");
-        InputStream is = jarFile.getInputStream(entry);
+    private static InputStream extractJarFile(InputStream input) throws IOException {
+        ZipInputStream zis = new ZipInputStream(input);
 
-        return new FilterInputStream(is) {
-            @Override
-            public void close() throws IOException {
-                super.close();
-                jarFile.close();
+        ZipEntry entry;
+
+        while ((entry = zis.getNextEntry()) != null) {
+            if (entry.getName().equals("classes.dex")) {
+                return zis;
             }
-        };
+
+            zis.closeEntry();
+        }
+        throw new RuntimeException("did not find classes.dex");
     }
 
 
