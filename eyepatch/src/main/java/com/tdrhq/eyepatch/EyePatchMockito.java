5c21c23141daa8a63c76a6a2a5fcb9857cb537c3
Arnold Noronha <arnold@tdrhq.com>
2018-08-15 16:16:06 -0400

    minor renaming

--- eyepatch/src/main/java/com/tdrhq/eyepatch/EyePatchMockito.java
+++ eyepatch/src/main/java/com/tdrhq/eyepatch/EyePatchMockito.java
@@ -6,16 +6,16 @@ public class EyePatchMockito {
     public static Class verifyStaticClass = null;
 
     public static void verifyStatic(Class klass) {
-        StaticVerificationHandler handler = getStaticVerificationHalder(klass);
+        StaticVerificationHandler handler = getStaticVerificationHandler(klass);
         handler.verifyStatic(klass);
     }
 
     public static void resetStatic(Class klass) {
-        StaticVerificationHandler handler = getStaticVerificationHalder(klass);
+        StaticVerificationHandler handler = getStaticVerificationHandler(klass);
         handler.resetStatic(klass);
     }
 
-    private static StaticVerificationHandler getStaticVerificationHalder(Class klass) {
+    private static StaticVerificationHandler getStaticVerificationHandler(Class klass) {
         ClassLoader classLoader = klass.getClassLoader();
         if (!(classLoader instanceof StaticVerificationHandler)) {
             throw new IllegalArgumentException(
