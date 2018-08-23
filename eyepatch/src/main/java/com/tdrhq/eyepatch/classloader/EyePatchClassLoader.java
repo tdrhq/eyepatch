22b1257be5e46da24eefe49efc3f89adcc6e2b3e
Arnold Noronha <arnold@tdrhq.com>
2018-08-23 18:16:50 -0400

    hide getMockables

--- eyepatch/src/main/java/com/tdrhq/eyepatch/classloader/EyePatchClassLoader.java
+++ eyepatch/src/main/java/com/tdrhq/eyepatch/classloader/EyePatchClassLoader.java
@@ -54,7 +54,7 @@ public class EyePatchClassLoader extends ClassLoader
                 .newInstance(classHandlerProvider);
     }
 
-    public Set<String> getMockables() {
+    private Set<String> getMockables() {
         return mockables;
     }
 
