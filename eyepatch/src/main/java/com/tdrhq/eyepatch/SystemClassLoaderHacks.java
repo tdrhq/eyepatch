abe721ac4619ae3f9f1c96bb9c6e9ad219c9e665
Arnold Noronha <arnold@tdrhq.com>
2018-08-24 10:54:05 -0400

    ...

--- eyepatch/src/main/java/com/tdrhq/eyepatch/SystemClassLoaderHacks.java
+++ eyepatch/src/main/java/com/tdrhq/eyepatch/SystemClassLoaderHacks.java
@@ -1,6 +1,7 @@
 package com.tdrhq.eyepatch;
 
 import android.content.Context;
+import android.os.Build;
 
 import com.tdrhq.eyepatch.util.Whitebox;
 
@@ -10,10 +11,24 @@ public class SystemClassLoaderHacks {
     }
 
     public static void validateClassLoaderCaches(Context context) {
-            ClassLoader expected = (ClassLoader) Whitebox.getStaticField(
-                    SYSTEM_CLASS_LOADER(),
-                    "loader"
-            );
+        ClassLoader expected = (ClassLoader) Whitebox.getStaticField(
+                SYSTEM_CLASS_LOADER(),
+                "loader"
+        );
+
+        try {
+            Class contextImpl = Class.forName("android.app.ContextImpl");
+
+            if (Build.VERSION.SDK_INT >= 26) {
+                ClassLoader actual = (ClassLoader) Whitebox.getField(context, contextImpl, "mClassLoader");
+                if (actual != null && expected != actual) {
+                    throw new RuntimeException("ContextImpl/mClassLoader is out of sync");
+                }
+            }
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
